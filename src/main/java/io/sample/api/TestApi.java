7bef429af7227243d588e4d074d3e9a921354f45
Azad Ramasamy <Azad Ramasamy>
2017-01-19 02:31:25 -0500

    hello world

---
+++ src/main/java/io/sample/api/TestApi.java
@@ -0,0 +1,15 @@
+package io.sample.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(TestApi.class, args);
+
+	}
+
+}
